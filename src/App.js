1ac11b8825ab992ff4ee2ac7d49dc870342ac924
erntorii <erntorii@gmail.com>
2021-06-18 22:15:54 +0900

    add TaskInput

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
+import TaskInput from './features/task/TaskInput';
 import TaskList from './features/task/TaskList';
 import './App.css';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Counter />
+        <TaskInput />
         <TaskList />
       </header>
     </div>
